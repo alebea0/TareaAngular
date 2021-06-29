d2f3db0d81ff2445d9e5ddae89f1c2ba27e3b929
alebea0 <17470044@campeche.tecnm.mx>
2021-06-29 11:46:25 -0500

    Se agrego Summary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
+import { StocksService } from './services/stocks.service';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-
+import { SummaryComponent } from './components/summary/summary.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ declarations: [
+ AppComponent,
+ SummaryComponent
+ ],
+ imports: [
+ BrowserModule,
+ HttpClientModule
+ ],
+ providers: [StocksService],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
